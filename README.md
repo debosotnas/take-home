9a8f912e76690f279f55cbc22884fa5a86713ced
Obed <obed.santos@plastiq.com>
2021-08-03 11:23:41 -0300

    Initial local env setup

---
+++ README.md
@@ -0,0 +1,28 @@
+# Take Home Project
+
+Take Home is an interview project for Hero.
+
+## Installation
+
+Use node package manager to install Take Home dependencies [npm](https://www.npmjs.com/).
+
+```bash
+npm install
+```
+
+## Run project
+
+```javascript
+npm start
+```
+## Build project dist
+
+```javascript
+npm run build
+```
+
+## Sources and Further reading
+Following link are recommend to read about details on tools used for this project.
+- abc
+- def
+- jkl
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { HotModuleReplacementPlugin } = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "./dist"),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new CleanWebpackPlugin(),
+    new HotModuleReplacementPlugin(),
+  ],
+};
